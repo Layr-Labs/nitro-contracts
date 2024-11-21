2206f94a83767829c5f2b6063c3c181a3654ae86
Ethen Pociask <ethen@eigenlabs.org>
2024-11-21 22:10:51 +0700

    fix: Update blob verifier injection pattern - bring back _getPrescaledAmount function

--- scripts/rollupCreation.ts
+++ scripts/rollupCreation.ts
@@ -4,7 +4,7 @@ import { run } from 'hardhat'
 import { abi as rollupCreatorAbi } from '../build/contracts/src/rollup/RollupCreator.sol/RollupCreator.json'
 import { config, maxDataSize } from './config'
 import { BigNumber, Signer } from 'ethers'
-import { IERC20__factory } from '../build/types'
+import { ERC20, ERC20__factory, IERC20__factory } from '../build/types'
 import { sleep } from './testSetup'
 import { promises as fs } from 'fs'
 import { _isRunningOnArbitrum, verifyContract } from './deploymentUtils'
@@ -341,4 +341,26 @@ async function _getDevRollupConfig(
       nonce: nonceHex,
     })
   }
+}
+
+async function _getPrescaledAmount(
+  nativeToken: ERC20,
+  amount: BigNumber
+): Promise<BigNumber> {
+  const decimals = BigNumber.from(await nativeToken.decimals())
+  if (decimals.lt(BigNumber.from(18))) {
+    const scalingFactor = BigNumber.from(10).pow(
+      BigNumber.from(18).sub(decimals)
+    )
+    let prescaledAmount = amount.div(scalingFactor)
+    // round up if needed
+    if (prescaledAmount.mul(scalingFactor).lt(amount)) {
+      prescaledAmount = prescaledAmount.add(BigNumber.from(1))
+    }
+    return prescaledAmount
+  } else if (decimals.gt(BigNumber.from(18))) {
+    return amount.mul(BigNumber.from(10).pow(decimals.sub(BigNumber.from(18))))
+  }
+
+  return amount
 }
