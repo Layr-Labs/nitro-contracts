a1e3d98cc7036ae6c995c4bad09dff6c6c8c9a80
Ethen Pociask <ethen@eigenlabs.org>
2025-01-17 20:00:17 +0700

    chore: Decrement storage buffer, add EigenDA delay proof entrypoint, & refactor EigenDA tests - update gap__ to ensure rollup manager still lives at slot 50

--- src/bridge/SequencerInbox.sol
+++ src/bridge/SequencerInbox.sol
@@ -146,7 +146,7 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
     // gap used to ensure forward compatiblity with newly introduced storage variables
     // from upstream offchainlabs/nitro-contracts. Any newly introduced storage vars
     // made in subsequent releases should result in decrementing the gap counter
-    uint256[37] internal __gap;
+    uint256[36] internal __gap;
     IRollupManager public eigenDARollupManager;
 
     constructor(
