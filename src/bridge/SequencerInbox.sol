41a23971487da781357575946f1a281b5ffe0839
Shyam Patel <shyampkira@gmail.com>
2026-01-05 06:12:24 -0800

    Add ALT DA certificate (0x01) support to SequencerInbox
    Add DAC_CERTIFICATE_MESSAGE_HEADER_FLAG constant and update validation to allow header byte 0x01 for ReferenceDA/CustomDA certificates following ALT DA spec.

--- src/bridge/SequencerInbox.sol
+++ src/bridge/SequencerInbox.sol
@@ -91,6 +91,9 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
     /// @inheritdoc ISequencerInbox
     bytes1 public constant ZERO_HEAVY_MESSAGE_HEADER_FLAG = 0x20;
 
+    /// @inheritdoc ISequencerInbox
+    bytes1 public constant DAC_CERTIFICATE_MESSAGE_HEADER_FLAG = 0x01;
+
     // GAS_PER_BLOB from EIP-4844
     uint256 internal constant GAS_PER_BLOB = 1 << 17;
 
@@ -707,7 +710,8 @@ contract SequencerInbox is DelegateCallAware, GasRefundEnabled, ISequencerInbox 
     ) internal pure returns (bool) {
         return headerByte == BROTLI_MESSAGE_HEADER_FLAG || headerByte == DAS_MESSAGE_HEADER_FLAG
             || (headerByte == (DAS_MESSAGE_HEADER_FLAG | TREE_DAS_MESSAGE_HEADER_FLAG))
-            || headerByte == ZERO_HEAVY_MESSAGE_HEADER_FLAG;
+            || headerByte == ZERO_HEAVY_MESSAGE_HEADER_FLAG
+            || headerByte == DAC_CERTIFICATE_MESSAGE_HEADER_FLAG;
     }
 
     /// @dev    Form a hash of the data taken from the calldata
